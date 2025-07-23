5e4bbcc82dcd8876914cc05e2688773253d36229
gouhara takuya <241126@std.hi-joho.ac.jp>
2025-07-23 09:27:56 +0900

    最終課題（テスト完了） インターフェース見直し済み

--- src/Creature.java
+++ src/Creature.java
@@ -1,8 +1,8 @@
 public interface Creature {
-    public abstract boolean isAlive();
-    public abstract void showStatus();
-    public abstract void attack(Creature target);
-    public abstract String getName();
-    public abstract int getHp();
-    public abstract void setHp(int hp);
+    /*public abstract*/ boolean isAlive();
+    /*public abstract*/ void showStatus();
+    /*public abstract*/ void attack(Creature target);
+    /*public abstract*/ String getName();
+    /*public abstract*/ int getHp();
+    /*public abstract*/ void setHp(int hp);
 }
